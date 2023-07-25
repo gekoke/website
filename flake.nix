9d05d0d6713c5981dadc749573999e18d25be65e
Gregor Grigorjan <gregor@grigorjan.net>
2023-07-25 03:46:02 +0300

    Push from flake output

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    resume.url = "github:gekoke/resume";
+  };
+
+  outputs = { nixpkgs, resume, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      packages.${system}.default = pkgs.stdenvNoCC.mkDerivation {
+        name = "website";
+        src = ./.;
+        buildPhase = ''
+        '';
+        installPhase = ''
+          mkdir -p $out/public
+          cp ${resume.packages.${system}.default}/resume.pdf src/* $out/public/
+        '';
+      };
+    };
+}
